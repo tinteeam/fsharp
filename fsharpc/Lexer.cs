8bdcaa263ca90b280a52e9daf7a30b443493d036
Kone <Kone@MATKA>
2024-04-24 09:07:04 +0300

    Add project files.

---
+++ fsharpc/Lexer.cs
@@ -0,0 +1,66 @@
+﻿namespace fsharpc
+{
+    enum SyntaxKind
+    {
+
+    }
+
+    class SyntaxToken
+    {
+
+
+
+        public SyntaxToken(SyntaxKind kind, int position, string text)
+        {
+            Kind = kind;
+        }
+        public SyntaxKind Kind { get; }
+
+        public int Position { get; }
+
+        public string Text { get; }
+
+
+    }
+
+    class Lexer
+    {
+        //readonly for _position
+        private readonly string _text;
+
+        private  int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+
+        }
+
+        private char Current
+        {
+            get
+            {
+                if (_position == _text.Length)
+                {
+                    return '\0';
+                }
+                return _text[_position];
+            }
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            if (char.IsDigit(Current) {
+                var start = _position;
+            }
+            
+                
+            
+        }
+    }
+}
---
+++ fsharpc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fsharpc
+{
+    //the main code for putting the compiler together
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("f#>");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return;
+
+                }
+                if (line == "1 + 2 * 3")
+                {
+                    Console.WriteLine("9");
+                }
+                else
+                {
+                    Console.WriteLine("error!");
+                }
+
+            }
+        }
+    }
+}
